ed95dd2041cc242a21fe5294bdb1bb15d834c89c
DThuyTech <dthuytech@gmail.com>
2023-06-22 10:43:53 +0700

    firstcm

---
+++ Areas/Identity/Data/AppHomeworkContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppHomework.Data;
+
+public class AppHomeworkContext : IdentityDbContext<IdentityUser>
+{
+    public AppHomeworkContext(DbContextOptions<AppHomeworkContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
